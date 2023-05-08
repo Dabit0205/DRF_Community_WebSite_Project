33059716f58e484c4635499d24949ce8d780836c
Park JongWon <jongwonparkk@gamil.com>
2023-05-08 19:42:51 +0900

    chore : update settings.py install apps

--- DRF_Community_WebSite_Project/settings.py
+++ DRF_Community_WebSite_Project/settings.py
@@ -39,11 +39,27 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user",
+    "article",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "corsheaders",
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5500"]  # live server
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
