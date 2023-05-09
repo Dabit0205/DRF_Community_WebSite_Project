c97a491fb5444942364750b514cf42a3bac46fc8
2017250297-choi <97ckdtn@gmail.com>
2023-05-09 19:59:59 +0900

    Feat : Add(use) TokenRefreshView, TokenVerifyView
    refresh token을 post request body로 받고 새 access을 발행하는 기본 제공 view
    refresh/access token을 post request body로 받고 유효한지 체크하는 기본제공 view
    (유효:200, 만료:401)

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,8 @@ from rest_framework_simplejwt.views import TokenVerifyView
 urlpatterns = [
     path("sign/", views.UserSignUpAndOutView.as_view(), name="signup/out"),
     path("token/", views.MyTokenObtaionVeiw.as_view(), name="token"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    # refresh /access token이 expire하지 않았는지 post 메소드로 request body에 실어 확인.
+    # {"token":"<토큰문자열>"} -> 유효할시 상태코드 200, 아닐시 401
+    path("verify/", TokenVerifyView.as_view(), name="verify"),
 ]
