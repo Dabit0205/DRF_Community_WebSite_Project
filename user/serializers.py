b01edd77a914911e636d4f15f82951c906c62244
2017250297-choi <97ckdtn@gmail.com>
2023-05-09 11:16:28 +0900

    Feat : Create Sign-up Feature
    1. 유저정보를 받아들이고 표시하기위한 시리얼라이저를 만들었습니다.
    2. 해당 시리얼라이저에서 검증 및 객체 저장을 위한 함수를 오버라이딩하였습니다.
    3. 검증을 오버라이딩함에 따라 비밀번호에 제약이 생겼습니다(8자리이상,한개 이상의 숫자,알파벳,특수문자(@$!%*#?&)모두 있어야함
    4. view 작성: UserSignUpAndOutView def post()
    5. url 연결: user/sign/

---
+++ user/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from .models import User
+import re
+
+PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    유저를 생성하거나 유저정보를 조회하기 위해 쓰이는 serializer이다.
+    생성할 땐 password,password2,username,email을 받고
+    조회할땐 username, email을 표시한다.
+
+    """
+
+    # 입력할 때만 쓰이는 password2 시리얼라이저필드 새로 정의
+    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = "__all__"
+        extra_kwargs = {
+            # write_only : 해당 필드를 쓰기 전용으로 만들어 준다.
+            # 쓰기 전용으로 설정 된 필드는 직렬화 된 데이터에서 보여지지 않는다.
+            "password": {"write_only": True},  # default : False
+        }
+
+    def create(self, validated_data):
+        """
+        부모 클래스의 create를 오버라이딩한다.
+        검증이후 실행됨
+        """
+        # 저장할 때는 모델에 password2 필드가 없으므로 제거
+        del validated_data["password2"]
+        user = super().create(validated_data)
+        user.set_password(validated_data["password"])  # 비밀번호 저장(해쉬)
+        user.save()
+        return user
+
+    def validate(self, attrs):
+        """
+        부모 클래스의 validate 오버라이딩
+        passwor와 password2가 일치하는지 확인
+        이외 나머지 검증은 부모 클래스의 validate에 맡김
+        """
+        if attrs.get("password") != attrs.get("password2"):
+            raise serializers.ValidationError({"password": "두 비멀번호가 일치하지 않습니다."})
+
+        if not re.match(PASSWORD_REGEX, attrs.get("password")):
+            raise serializers.ValidationError(
+                {
+                    "password": "비밀번호는 8자리 이상, 한개 이상의 숫자/알파벳/특수문자(@,$,!,%,*,#,?,&)로 이루어져야합니다."
+                }
+            )
+        return super().validate(attrs)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+
+from user.serializers import UserSerializer
+
 
 # Create your views here.
+class UserSignUpAndOutView(APIView):
+    """
+    사용자가 회원가입 하거나 회원 탈퇴를 하기 위한 APIView이다.
+    Post(가입)와 Delete(탈퇴) 요청만 받음.
+    """
+
+    # permission_classes = (IsExsistDeleteXorCreateOnly,)
+
+    def post(self, request):
+        """
+        회원가입을 위해선 username, email, password, password2를 입력받아야한다.
+        상태코드 201 / 생성된 유저 정보(username,email)반환
+        옳지 않은 입력(필수 입력 필드 빼먹거나 비밀번호 불일치)일 시 400 / error 반환
+        """
+        user_serialized = UserSerializer(data=request.data)
+        if user_serialized.is_valid():
+            user_serialized.save()
+            return Response(user_serialized.data, status=status.HTTP_201_CREATED)
+        return Response(user_serialized.errors, status=status.HTTP_400_BAD_REQUEST)
