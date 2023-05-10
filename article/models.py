d2c9742ad0072a1bf058cbbab015ff0a91c61c2e
Jeongchan-ho <zmxndk1@gmail.com>
2023-05-10 12:33:38 +0900

    Fix:Fix ArticleView
    1.article/midels.py author 필드의 on_delete를 수정
    2.article/views.py에 permission_class를 사용하게되어 ArticleView def post를 수정
    3.article/serializers.py를 수정하여 author_id를 db에 저장 할 수있게 수정하였습니다.

--- article/models.py
+++ article/models.py
@@ -7,13 +7,13 @@ from user.models import User
 class Article(models.Model):
     """
     Article 모델입니다,
-    author 필드는 User모델을 참조합니다, on_delete에 SET_NULL을 주어
-    회원이 삭제되어도 게시글은 남습니다.
+    author 필드는 User모델을 참조합니다, on_delete를 CASCADE로 변경하였습니다, user부분에서 user삭제를 비활성화로 처리하기에
+    유저가 작성한 게시글은 남게 됩니다. 그렇기에 db에서 유저 정보가 삭제되는 상황에서 함께 삭제되도록 변경하였습니다.
     title(게시글제목),content(게시글내용),created_at(게시글생성일),updated_at(게시글수정일)
     필드로 구성되어있습니다.
     """
 
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- article/serializers.py
+++ article/serializers.py
@@ -17,8 +17,18 @@ class ArticleCreateSerializer(serializers.ModelSerializer):
     """
     article/ url에 POST방식일 때 사용합니다.
     게시글을 작성할 때 사용합니다, title과 content 값이 필요합니다.
+    create를 오버라이딩하였습니다.
+    title과 content에 검증된 값을 넣습니다,db에 author_idr값을 넣기위해 view에서 context를 이용하여 받은 유저의 정보에서 user.id만 뽑아 author_id에 넣습니다.
     """
 
     class Meta:
         model = Article
         fields = ("title", "content")
+
+    def create(self, validated_data):
+        article = Article.objects.create(
+            title=validated_data["title"],
+            content=validated_data["content"],
+            author_id=self.context["request"].user.id,
+        )
+        return article
