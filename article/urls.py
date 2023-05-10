d45268f74611bf8549a1c2a81ceb3f79d1dc7709
Jeongchan-ho <zmxndk1@gmail.com>
2023-05-10 15:13:43 +0900

    Feat:Create Article Detail View
    1.article/views.py ArticleDetailView class가 추가되었습니다.
    2.권한부여를 위한 permissions.py가 생성되었습니다.
    3.serializer가 수정되었습니다. def get_author를 통해 username이 출력됩니다.
    4.article/urls.py에 ArticleDetailView를 위한 경로가 추가되었습니다.

--- article/urls.py
+++ article/urls.py
@@ -3,4 +3,9 @@ from article import views
 
 urlpatterns = [
     path("", views.ArticleView.as_view(), name="article_view"),
+    path(
+        "<int:article_id>/",
+        views.ArticleDetailView.as_view(),
+        name="article_detail_view",
+    ),
 ]
