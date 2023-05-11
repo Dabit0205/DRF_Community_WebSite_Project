2f12493d5fe9a6a3f9708e226cfa920adbd49739
Jeongchan-ho <zmxndk1@gmail.com>
2023-05-11 12:22:12 +0900

    Feat: CreatePagination
    1.페이지네이션 적용을 위해 settings.py에 REST_FRAMEWORK에 코드가 추가되었습니다.
    2.article/paginations.py 파일이 추가되었습니다.
    3.article/views.py의 ArticleView class가 수정되었습니다.

--- article/views.py
+++ article/views.py
@@ -1,39 +1,41 @@
 from rest_framework import status, permissions
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from article.models import Article
 from article.serializers import ArticleSerializer, ArticleCreateSerializer
 from article.permissions import IsOwnerOrReadOnly
+from article.paginations import ArticlePagination
+
 
 # Create your views here.
 
 
-class ArticleView(APIView):
+class ArticleView(generics.ListCreateAPIView):
     """
-    article/ url에서 사용됩니다.
-    ArticleView는 모든 게시물 출력, 게시물 등록에 사용됩니다.
-    get방식일 경우 저장된 모든 게시글을 보여줍니다,
-    post방식을 사용하면 게시글을 등록할 수 있습니다.
+    APIview에서는 페이지네이션 기능을 사용할 수 없었습니다, 그렇기에 generics를 사용했습니다.
+    ListCreateAPIView는 GET요청일 때 조회, POST요청일 때 새로운 데이터를 생성하는 역할을 합니다.
+    def get의 역할을 ListCreateAPIView에서 지원하기에 따로 지정해 줄 필요가 없습니다.
+    queryset을 통해 article의 내용을 가져옵니다.
+    필요한 것을 명시해주면 간단히 get 요청을 구현할 수 있습니다.
     """
 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    paginations_class = ArticlePagination
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
 
-    def get(self, request):
-        """
-        objects.all()을 통해 모델에 저장된 모든 것을 가져옵니다.
-        many=True를 사용하여 오류를 방지합니다.
-        성공 시 상태메시지 200을 출력합니다.
-        """
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         """
         토큰에서 유저 정보를 받을 수 있기 때문에 수정되었습니다.
         data 부분에는 게시글의 title,content를 받아 valid 작업을 받습니다.
         context에는 request요청을 한 유저의 정보를 받습니다. author_id를 저장하기 위해 사용되었습니다.
         serializer를 통해 검증된 정보를 만들어 return시켜줍니다.
+
+        +)형태의 변화는 없지만 ListCreateAPIView를 사용했기에
+        CreateAPIView를 오버라이딩 되었습니다.
+        부모클래스를 상속하기에 *args, **kwargs를 추가하여 오류를 예방하고
+        코드의 유연성을 높였습니다.
         """
         serializer = ArticleCreateSerializer(
             data=request.data, context={"request": request}
